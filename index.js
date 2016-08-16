6874c0ff3b3ec160f7e67a9942f269e147370bcc
Cameron <cameron.webley@gmail.com>
2016-08-16 18:29:28 -0500

    global variable simplification and removal of date.now fallback

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-var _win = typeof window !== 'undefined' ? window : null;
-var _pro = typeof process !== 'undefined' ? process : null;
+var _global = typeof window !== 'undefined' ? window : process;
 var _start;
 
-if (_win && _win.performance && _win.performance.now) {
+if (_global.performance && _global.performance.now) {
 	// In the browser with support
 	module.exports = perfNow;
-} else if (_pro && _pro.hrtime) {
+} else {
 	// In node use hrtime
 	_start = nano();
 	module.exports = perfNowNode;
-} else {
-	// fall back to simple data
-	_start = Date.now();
-	module.exports = perfNowDate;
 }
 
 function perfNow () {
-	return _win.performance.now();
+	return _global.performance.now();
 }
 
 function perfNowNode () {
 	return (nano() - _start) / 1e6;
 }
 
-function perfNowDate () {
-	return Date.now() - _start;
-}
-
 function nano () {
-	var hr = _pro.hrtime();
+	var hr = _global.hrtime();
 	return hr[0] * 1e9 + hr[1];
 }
